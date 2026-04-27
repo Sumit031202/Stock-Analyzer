b9394a41f0cbef3901f4f17b2ff1a837499c21e4
Sumit031202 <sumitkr.031202@gmail.com>
2026-04-28 03:09:39 +0530

    feat: added command line input for convenience

--- screenshot.js
+++ screenshot.js
@@ -20,13 +20,20 @@ async function run(){
     // i launched the chrome browser here
     const browser = await chromium.launch({headless:true});
 
-    const symbl="Reliance";
+    const symbl=process.argv[2];
+
+    // 2. Check if the user actually typed a name
+    if (!symbl) {
+        console.error("Please provide a stock symbol! Example: node screenshot.js RELIANCE");
+        process.exit(1);
+    }
+
     await takeScreenshot(browser,symbl,'D','daily.png');
     await takeScreenshot(browser,symbl,'W','weekly.png');
     await takeScreenshot(browser,symbl,'M','monthly.png');
 
     // close the browser
-    await browser.close;
+    await browser.close();
 }
 
 run().then(()=>{
